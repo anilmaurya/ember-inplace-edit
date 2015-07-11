379a770867deecacd16f498b1fa15e4f9fb3e321
Anil Maurya <anil@joshsoftware.com>
2015-07-11 22:26:18 +0530

    Focus editable field and set height when editing

--- addon/components/ember-inplace-edit.js
+++ addon/components/ember-inplace-edit.js
@@ -15,7 +15,27 @@ export default Ember.Component.extend({
         _this.send('doneEditing');
       });
     }
+    this.focus();
   },
+
+  height: null,
+
+  focus: function(){
+    var _this = this;
+    Ember.run.scheduleOnce('afterRender', this, function(){
+      if(_this.get('isEditing')){
+        if(_this.get('type') === 'input'){
+          $("#"+_this.get("elementId")+" input").focus();
+        }else{
+          $("#"+_this.get("elementId")+" textarea").css('height', _this.get('height'));
+          $("#"+this.get("elementId")+" textarea").focus();
+        }
+      }else{
+        // Set height of editable div
+         _this.set('height', $("#"+_this.get("elementId")).parent().css('height'));
+      }
+     })
+  }.observes('isEditing'),
 
   isTypeInput: function(){
     return this.get('type') === "input";
