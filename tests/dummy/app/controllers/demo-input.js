3367ddc1da5a7d03c722e99a1e337f5f59e84a60
Peter Toth <tothpeter08@gmail.com>
2015-12-29 18:18:30 +0100

    Start working on a proper demo page - Separate input and textarea examples into separate routes

---
+++ tests/dummy/app/controllers/demo-input.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  sampleText1: 'One line Text',
+
+  users: Ember.computed(function() {
+    return [
+      {
+        name: 'Jane Doe',
+        age: 30,
+        location: 'Miami, Florida'
+      },
+      {
+        name: 'John Doe',
+        age: 34,
+        location: 'Miami, Florida'
+      },
+      {
+        name: 'Toma',
+        age: 27,
+        location: 'Budapest, Hungary'
+      }
+    ];
+  }),
+
+  actions: {
+    updateUser(model, newValue) {
+      console.log('User updated: ', model);
+      console.log('New value: ', newValue);
+    },
+
+    inputActivated($container) {
+      $container.addClass('activated');
+    }
+  }
+});
