3826292b50730f322f7e3ebf00a4174002e98b1c
Peter Toth <tothpeter08@gmail.com>
2015-12-29 16:31:40 +0100

    Add bootstrap to the demo

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,11 +7,18 @@ module.exports = function(defaults) {
   });
 
   /*
-    This build file specifes the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
+  This build file specifes the options for the dummy test app of this
+  addon, located in `/tests/dummy`
+  This build file does *not* influence how the addon or the app using it
+  behave. You most likely want to be modifying `./index.js` or app's build file
   */
+
+
+  // CSS
+  app.import(app.bowerDirectory + '/bootstrap/dist/css/bootstrap.css');
+
+  // Scripts
+  app.import(app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js');
 
   return app.toTree();
 };
