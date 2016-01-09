19fde50bc35cc2e5cc46c6473925c6e07ec56b45
Anil <anilmaurya8dec@gmail.com>
2016-01-10 04:20:44 +0530

    Add acceptance tests for inplace input box edit

---
+++ tests/acceptance/input-test.js
@@ -0,0 +1,57 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from '../../tests/helpers/start-app';
+
+module('Acceptance | input', {
+  beforeEach: function() {
+    this.application = startApp();
+  },
+
+  afterEach: function() {
+    Ember.run(this.application, 'destroy');
+  }
+});
+
+test('show edit button on hover', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(find('.examples-header~.row:first .edit').hasClass('hide'), true);
+    triggerEvent('.examples-header~.row:first .edit-box', 'mouseenter');
+    andThen(function(){
+      assert.equal(find('.examples-header~.row:first .edit').hasClass('hide'), false);
+    });
+  });
+});
+
+test('show input box when click on edit button', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    triggerEvent('.examples-header~.row:first .edit-box', 'mouseenter');
+    andThen(function(){
+      click('.examples-header~.row:first .edit button');
+      andThen(function(){
+        assert.equal(find('.examples-header~.row:first input').length, 1);
+      });
+    });
+  });
+});
+
+test('edit text', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    triggerEvent('.examples-header~.row:first .edit-box', 'mouseenter');
+    andThen(function(){
+      click('.examples-header~.row:first .edit button');
+      andThen(function(){
+        fillIn('.examples-header~.row:first input', 'Updated content');
+        triggerEvent('.examples-header~.row:first input', 'focusout');
+        andThen(function(){
+          assert.equal(find('.examples-header~.row:first .edit-box').text().trim(), 'Updated content');
+        });
+      });
+    });
+  });
+});
