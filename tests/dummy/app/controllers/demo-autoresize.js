f0c29ba9deb9521bf796216b91a917423c51f9e0
Peter Toth <tothpeter08@gmail.com>
2016-03-27 17:25:39 +0200

    Add address autoresize example to dummy

--- tests/dummy/app/controllers/demo-autoresize.js
+++ tests/dummy/app/controllers/demo-autoresize.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   multiLineText: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, \n\nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  multiLineText2: '2nd Lorem ipsum dolor sit amet, consectetur adipisicing elit, \n\nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  multiLineText2: '2nd Lorem ipsum dolor sit amet, consectetur adipisicing elit, \n\nsed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+
+  city: 'Miami Beach',
+  state: 'Florida',
+  zip: '33139'
 });
